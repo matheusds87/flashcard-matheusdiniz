87b43046ed557ca23992f6985786fcdf2b131774
matheusds87 <164523152+matheusds87@users.noreply.github.com>
2024-11-01 11:29:26 -0300

    final

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'fortinte é um ?',
+    ' Battle royale'
+)
+
+criaCartao(
+    'Geografia',
+    'que salve o mundo?',
+    'modo de jogo de sobrevivencia de zombie'
+)
+
+criaCartao(
+    'Programação',
+    'qual o arma mais legal?',
+    'armas da temporada 2 do capitulo 2'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como o midas vai voltar de temporada remix 2?',
+    'remix com minascoculos'
+)
